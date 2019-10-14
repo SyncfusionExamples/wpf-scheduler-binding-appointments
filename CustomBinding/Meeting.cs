8e33855c88dc6bc087edf8c6d01d01a3f0fbc85e
Mohanraj-Guna <45057763+Mohanraj-Guna@users.noreply.github.com>
2019-10-14 17:56:54 +0530

    Samples are committed for custom binding

---
+++ CustomBinding/Meeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CustomBinding
+{
+    /// <summary>   
+    /// Represents custom data properties.   
+    /// </summary>   
+    public class Meeting
+    {
+        public string EventName { get; set; }
+        public string Organizer { get; set; }
+        public string ContactID { get; set; }
+        public int Capacity { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public Brush Color { get; set; }
+        public bool AllDay { get; set; }
+    }
+}
